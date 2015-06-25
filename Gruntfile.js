7828db288880c75d24794abcf8ba1b7ff689bf0d
Hilscher, Robert <robert.hilscher@pearson.com>
2015-06-25 14:11:12 +0200

    Update unit tests

--- Gruntfile.js
+++ Gruntfile.js
@@ -56,7 +56,7 @@ module.exports = function(grunt) {
 
   // Whenever the "test" task is run, first clean the "tmp" dir, then run this
   // plugin's task(s), then test the result.
-  grunt.registerTask('test', ['clean', 'protractor_cucumber_html-report', 'nodeunit']);
+  grunt.registerTask('test', ['clean', 'protractor-cucumber-html-report', 'nodeunit']);
 
   // By default, lint and run all tests.
   grunt.registerTask('default', ['jshint', 'protractor-cucumber-html-report']);
--- test/protractor_cucumber_html_report_test.js
+++ test/protractor_cucumber_html_report_test.js
@@ -2,25 +2,6 @@
 
 var grunt = require('grunt');
 
-/*
-  ======== A Handy Little Nodeunit Reference ========
-  https://github.com/caolan/nodeunit
-
-  Test methods:
-    test.expect(numAssertions)
-    test.done()
-  Test assertions:
-    test.ok(value, [message])
-    test.equal(actual, expected, [message])
-    test.notEqual(actual, expected, [message])
-    test.deepEqual(actual, expected, [message])
-    test.notDeepEqual(actual, expected, [message])
-    test.strictEqual(actual, expected, [message])
-    test.notStrictEqual(actual, expected, [message])
-    test.throws(block, [error], [message])
-    test.doesNotThrow(block, [error], [message])
-    test.ifError(value)
-*/
 
 exports.protractor_cucumber_html_report = {
   setUp: function(done) {
@@ -29,20 +10,9 @@ exports.protractor_cucumber_html_report = {
   },
   default_options: function(test) {
     test.expect(1);
-
-    var actual = grunt.file.read('tmp/default_options');
+    var actual = grunt.file.read('tmp/report.html');
     var expected = grunt.file.read('test/expected/default_options');
     test.equal(actual, expected, 'should describe what the default behavior is.');
-
     test.done();
-  },
-  custom_options: function(test) {
-    test.expect(1);
-
-    var actual = grunt.file.read('tmp/custom_options');
-    var expected = grunt.file.read('test/expected/custom_options');
-    test.equal(actual, expected, 'should describe what the custom option(s) behavior is.');
-
-    test.done();
-  },
+  }
 };
